c8e1cdd2f7b20038964748166940521043aae0e4
Renaldi Najwan Fauzan <renaldinajwanfauzan@gmail.com>
2025-04-30 09:18:52 +0700

    Initial Commit

---
+++ iostestone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iostestone
+//
+//  Created by Renaldi Najwan Fauzan on 30/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iostestoneTests/iostestoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  iostestoneTests.swift
+//  iostestoneTests
+//
+//  Created by Renaldi Najwan Fauzan on 30/04/25.
+//
+
+import Testing
+@testable import iostestone
+
+struct iostestoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
